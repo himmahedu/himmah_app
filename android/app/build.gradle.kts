1f5285ececd5b3240e72578bf0dfdbc46c6be994
همة التعليمية <himmahedu@gmail.com>
2026-05-18 15:50:09 +0300

    Create build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.himmah_app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.himmah_app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
